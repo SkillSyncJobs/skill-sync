d1327f02d12c9e2d35e6c85bc90875d1e65d5d5d
Punit <spunit2025@gmail.com>
2025-01-01 22:00:08 +0530

    added all the regions in student signup

--- Frontend/src/pages/auth/StudentSignup.jsx
+++ Frontend/src/pages/auth/StudentSignup.jsx
@@ -139,7 +139,7 @@ const StudentSignup = () => {
                     <div className="flex flex-col gap-1">
                         <label className="text-sm text-gray-600">Date of Birth</label>
                         <input
-                            type="text"
+                            type="date"
                             className="px-4 py-2 rounded-xl border border-[#BCC3D0] w-[232px]"
                             placeholder="01-01-2000"
                             name="date_of_birth"
@@ -180,8 +180,43 @@ const StudentSignup = () => {
                                 Select state
                             </option>
                             {/* Add other state options */}
-                            <option value="delhi">Delhi</option>
-                            <option value="maharashtra">Maharashtra</option>
+                            <option value="andaman-nicobar">Andaman and Nicobar Islands</option>
+                                <option value="andhra-pradesh">Andhra Pradesh</option>
+                                <option value="arunachal-pradesh">Arunachal Pradesh</option>
+                                <option value="assam">Assam</option>
+                                <option value="bihar">Bihar</option>
+                                <option value="chandigarh">Chandigarh</option>
+                                <option value="chhattisgarh">Chhattisgarh</option>
+                                <option value="dadra-nagar-haveli-daman-diu">Dadra and Nagar Haveli and Daman and Diu</option>
+                                <option value="delhi">Delhi</option>
+                                <option value="goa">Goa</option>
+                                <option value="gujarat">Gujarat</option>
+                                <option value="haryana">Haryana</option>
+                                <option value="himachal-pradesh">Himachal Pradesh</option>
+                                <option value="jammu-kashmir">Jammu and Kashmir</option>
+                                <option value="jharkhand">Jharkhand</option>
+                                <option value="karnataka">Karnataka</option>
+                                <option value="kerala">Kerala</option>
+                                <option value="ladakh">Ladakh</option>
+                                <option value="lakshadweep">Lakshadweep</option>
+                                <option value="madhya-pradesh">Madhya Pradesh</option>
+                                <option value="maharashtra">Maharashtra</option>
+                                <option value="manipur">Manipur</option>
+                                <option value="meghalaya">Meghalaya</option>
+                                <option value="mizoram">Mizoram</option>
+                                <option value="nagaland">Nagaland</option>
+                                <option value="odisha">Odisha</option>
+                                <option value="puducherry">Puducherry</option>
+                                <option value="punjab">Punjab</option>
+                                <option value="rajasthan">Rajasthan</option>
+                                <option value="sikkim">Sikkim</option>
+                                <option value="tamil-nadu">Tamil Nadu</option>
+                                <option value="telangana">Telangana</option>
+                                <option value="tripura">Tripura</option>
+                                <option value="uttar-pradesh">Uttar Pradesh</option>
+                                <option value="uttarakhand">Uttarakhand</option>
+                                <option value="west-bengal">West Bengal</option>
+
                         </select>
                     </div>
                 </div>
